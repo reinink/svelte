9a1ef48e70b742128f351eb96a010689ee0d0201
Jonathan Reinink <jonathan@reinink.ca>
2019-04-23 08:43:21 -0400

    Build out Inertia link

--- resources/js/Shared/Link.svelte
+++ resources/js/Shared/Link.svelte
@@ -1,7 +1,21 @@
 <script>
-import Inertia from 'inertia'
+import Inertia, { shouldIntercept } from 'inertia'
+
+export let href,
+  method = 'get',
+  replace = false,
+  preserveScroll = false
+
+function visit(event) {
+  if (shouldIntercept(event)) {
+    event.preventDefault()
+    Inertia.visit(href, { method, replace, preserveScroll })
+  }
+}
 </script>
 
-<a href="{$$props.href}" class={$$props.class} on:click|preventDefault={ () => Inertia.visit($$props.href) }>
+<a href="{href}" {...$$props} on:click={visit}>
   <slot/>
 </a>
+
+
